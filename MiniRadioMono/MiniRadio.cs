a8200b927e91b44c4287e3c2f37ea36205bfc48a
Hottabxp <hottabxs@gmail.com>
2016-02-13 10:50:21 +0000

    Начата разработка

---
+++ MiniRadioMono/MiniRadio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+public class MiniRadio : Form
+	{
+
+	public static void Main()
+	{
+		Application.Run (new MiniRadio ());
+	}
+
+		public MiniRadio ()
+		{
+		}
+	}
+
+
